05c61e306213e94b813db7a13d35d084d5ff6674
Tomer Segal <tomer.segal@sensi.ai>
2023-09-15 14:22:51 +0300

    adding describe

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,18 +1,22 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
+test.describe('login scenarios', () => {
+  
+  test('has title', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+  
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Swag Labs/);
+  });
+  
+  test('valid login', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('[data-test="username"]').click();
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').click();
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+    await page.getByText('Products').click();
+  });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Swag Labs/);
-});
-
-test('valid login', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
-  await page.getByText('Products').click();
-});
+})
