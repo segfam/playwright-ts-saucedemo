4445738c6b06271a85151b247ecf7fd4c2cefae8
Tomer Segal <tomer.segal@sensi.ai>
2023-09-15 14:18:44 +0300

    adding 2 tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -6,3 +6,13 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Swag Labs/);
 });
+
+test('valid login', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.getByText('Products').click();
+});
